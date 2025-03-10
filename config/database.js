ffadfd85d8b70a5228095ecd6785f34adf79ba60
Mustafiz <mustafizansari35@gmail.com>
2025-03-10 17:16:35 +0530

    Refactor database configuration for production environment with SSL support

--- config/database.js
+++ config/database.js
@@ -5,16 +5,29 @@ const { Sequelize } = require('sequelize');
 
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+const isProduction = process.env.NODE_ENV === "production";
+const sequelize = isProduction
+  ? new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Needed for Render PostgreSQL SSL
+      },
+    },
     logging: false,
-  }
-);
+  })
+  : new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: "postgres",
+      logging: false,
+    }
+  );
 
 module.exports = sequelize;
