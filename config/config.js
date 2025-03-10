c163ba63a0cebf8acc5fd6cf47ab7b444579e607
Mustafiz <mustafizansari35@gmail.com>
2025-03-10 17:34:47 +0530

    Update production database configuration to use DATABASE_URL with SSL support

--- config/config.js
+++ config/config.js
@@ -19,11 +19,13 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres'
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    }
   }
 }
